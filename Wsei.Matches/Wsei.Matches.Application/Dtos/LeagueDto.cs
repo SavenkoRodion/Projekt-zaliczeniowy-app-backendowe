a4054b6fa93dea94fa4c0e854d01b9dde2a4240c
RodionSavenko <savenko662@gmail.com>
2023-06-14 22:15:42 +0200

    Fix dtos

--- Wsei.Matches/Wsei.Matches.Application/Dtos/LeagueDto.cs
+++ Wsei.Matches/Wsei.Matches.Application/Dtos/LeagueDto.cs
@@ -5,7 +5,5 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public CountryDto? Country { get; set; }
-        public IEnumerable<TeamDto> Teams { get; set; }
-        public IEnumerable<MatchDto> Matches { get; set; }
     }
 }

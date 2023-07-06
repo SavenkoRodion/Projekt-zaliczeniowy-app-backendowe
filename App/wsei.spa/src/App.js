e81495bc3db3e20b55bf1198fb5d7e738d3f6e99
RodionSavenko <savenko662@gmail.com>
2023-07-06 18:03:10 +0200

    Early ui implementation

---
+++ App/wsei.spa/src/App.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react"
+import Header from "./template/Header"
+import Country from "./pages/Counrty/Country";
+import Matches from "./pages/Matches/Matches";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+const App = () => {
+
+  return (
+    <Router>
+        <div>
+          <h2>Welcome to React Router Tutorial</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/lol'} className="nav-link">Contact</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Routes>
+              <Route exact path='/' component={Country} />
+              <Route path='/lol' component={Matches} />
+          </Routes>
+        </div>
+      </Router>
+  );
+}
+
+export default App;
+
+/* <Header></Header>
+<Routes>
+    <Route path="/Matches">
+      <Matches />
+    </Route>
+    <Route path="/Country">
+      <Country />
+    </Route>
+  </Routes> */

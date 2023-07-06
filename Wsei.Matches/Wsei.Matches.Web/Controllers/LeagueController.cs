ac1943940b77ee011961b8471a8c095e747fcb82
RodionSavenko <savenko662@gmail.com>
2023-07-06 18:36:10 +0200

    Implement league controller

---
+++ Wsei.Matches/Wsei.Matches.Web/Controllers/LeagueController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Wsei.Matches.Application.Dtos;
+using Wsei.Matches.Core.Interfaces;
+
+namespace Wsei.Matches.Web.Controllers;
+
+[ApiController]
+[Route("/league")]
+public class LeagueController : BaseCrudController<LeagueDto>
+{
+    public LeagueController(IRepository<LeagueDto> repository) : base(repository) { }
+}
+
+

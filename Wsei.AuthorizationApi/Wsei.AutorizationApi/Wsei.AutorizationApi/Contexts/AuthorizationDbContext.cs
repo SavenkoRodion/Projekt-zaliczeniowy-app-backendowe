379741a2c52b7f059f2cf219ae7698303b3fbe25
Adrian Smoła <Adrian.Smola@microsoft.wsei.edu.pl>
2023-07-06 20:20:14 +0200

    Implementation

---
+++ Wsei.AuthorizationApi/Wsei.AutorizationApi/Wsei.AutorizationApi/Contexts/AuthorizationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Wsei.AutorizationApi.Controllers
+{
+    public class AuthorizationDbContext : DbContext
+    {
+        public AuthorizationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}

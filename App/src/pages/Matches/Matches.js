cf4c1cf1dcc5f59eab67ec80eecadd5d90776472
RodionSavenko <savenko662@gmail.com>
2023-07-08 19:55:32 +0200

    Implement some ui improvements

--- App/src/pages/Matches/Matches.js
+++ App/src/pages/Matches/Matches.js
@@ -1,35 +1,48 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 
 const Matches = () => {
-  const [users, setUsers] = useState([])
+  const [matches, setMatches] = useState([]);
 
   const fetchUserData = () => {
-    fetch("https://localhost:7185/matches/all")
-      .then(response => {
-        console.log(response)
-        return response.json()
+    fetch("https://localhost:7185/match/all")
+      .then((response) => {
+        console.log(response);
+        return response.json();
       })
-      .then(data => {
-        setUsers(data)
-      })
-  }
+      .then((data) => {
+        setMatches(data);
+        console.log(matches);
+        console.log(matches.length);
+        matches.forEach((match) =>
+          console.log(match.homeTeam.name + "-" + match.homeTeam.name)
+        );
+      });
+  };
 
   useEffect(() => {
-    fetchUserData()
-  }, [])
+    fetchUserData();
+  }, []);
 
   return (
     <div>
       lolek
-      {users.length > 0 && (
+      {matches.length > 0 && (
         <ul>
-          {users.map(user => (
-            <li key={user.id}>{user.name}</li>
+          {matches.map((match) => (
+            <li>
+              {match.homeTeam.name} - {match.guestTeam.name}
+            </li>
           ))}
         </ul>
       )}
     </div>
   );
-}
+};
 
 export default Matches;
+
+// {matches.forEach((match) => (
+//   <li key={match.id}>
+//     {match.homeTeam.name} {match.homeTeam.name}
+//   </li>
+// ))}

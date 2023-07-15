2bf5cced5a56ddcc2f9859e40de996e84ec4570a
RodionSavenko <savenko662@gmail.com>
2023-07-16 01:40:38 +0200

    Implementation

--- App/src/pages/Counrty/Country.js
+++ App/src/pages/Counrty/Country.js
@@ -1,29 +1,29 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 
 function Country() {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
   const fetchUserData = () => {
-    fetch("https://localhost:7185/country/all")
-      .then(response => {
-        console.log(response)
-        return response.json()
+    fetch("https://localhost:7185/country/")
+      .then((response) => {
+        console.log(response);
+        return response.json();
       })
-      .then(data => {
-        setUsers(data)
-      })
-  }
+      .then((data) => {
+        setUsers(data);
+      });
+  };
 
   useEffect(() => {
-    fetchUserData()
-  }, [])
+    fetchUserData();
+  }, []);
 
   return (
     <div>
       lol
       {users.length > 0 && (
         <ul>
-          {users.map(user => (
+          {users.map((user) => (
             <li key={user.id}>{user.name}</li>
           ))}
         </ul>
